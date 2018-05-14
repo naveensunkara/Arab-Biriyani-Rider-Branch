62bbb1c29fa533a62e0d4f87ee1976ce1766168c
Naveen <naveen241193@gmail.com>
2018-05-14 23:16:41 +0530

    34545345

--- src/pages/notification/notification.ts
+++ src/pages/notification/notification.ts
@@ -25,6 +25,22 @@ export class NotificationPage {
             title: 'Packed for Delivery',
             date: '24, Apr, 2018 08:40 AM',
             order: 'Purchase Order #1100010 is packed for delivery.'
+        },{
+            title: 'Order Accepted',
+            date: '25, Apr, 2018 11:47 PM',
+            order: 'Purchase Order #1100011 Accepted'
+        },{
+            title: 'Delivered',
+            date: 'On 24, Apr, 2018 09:30 AM',
+            order: 'Purchase Order #1100010 Delivered to you'
+        },{
+            title: 'Order Dispatched',
+            date: '24, Apr, 2018 08:40 AM',
+            order: 'Purchase Order #1100010 dispatched. Our rider is on the way'
+        },{
+            title: 'Packed for Delivery',
+            date: '24, Apr, 2018 08:40 AM',
+            order: 'Purchase Order #1100010 is packed for delivery.'
         }
     ]
     constructor(public navCtrl: NavController) { }
